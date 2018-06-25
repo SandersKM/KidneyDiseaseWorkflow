2f43cef134ed9c61f97f2b3a16292c536465a5eb
Kate Sanders <SandersKM@Hendrix.edu>
2018-06-25 14:09:26 -0400

    Organized entries

--- GatherGenes.R
+++ GatherGenes.R
@@ -1,4 +1,4 @@
-
+library(rvest)
 library(biomaRt)
 library(gwascat)
 data(ebicat37)
@@ -42,8 +42,9 @@ gene_file$description <- extract_from_esummary(gene_file$summary, "description")
 gene_file$chromosome <- extract_from_esummary(gene_file$summary, "chromosome")
 gene_file$map.location <- extract_from_esummary(gene_file$summary, "maplocation")
 gene_file$mim <- extract_from_esummary(gene_file$summary, "mim")
-gene_file$exon.count <- lapply(1:dim(gene_file)[1], function(x){extract_from_esummary(gene_file$summary,
-                                                                          "genomicinfo")[[x]]$exoncount})
+gene_file$exon.count <- sapply(1:dim(gene_file)[1], function(x){as.integer(extract_from_esummary(
+  gene_file$summary,"genomicinfo")[[x]]$exoncount)})
+gene_file$exon.count <- sapply(gene_file$exon.count, as.numeric)
 gene_file$summary <- extract_from_esummary(gene_file$summary, "summary")
 
 
@@ -51,82 +52,37 @@ gene_file$summary <- extract_from_esummary(gene_file$summary, "summary")
 ensembl = useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh=37)
 # get ensemblIDs
 get_ensemblID <- function(n){
-  id <- getBM("ensembl_gene_id", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
+  id <- getBM("ensembl_gene_id", filters="hgnc_symbol", values=gene_file$name[n], mart=ensembl)
   return(id)
 }
-gene_file$EnsemblID <- sapply(1:dim(gene_file)[1], get_ensemblID)
+gene_file$EnsemblID <- as.character(sapply(1:dim(gene_file)[1], get_ensemblID))
 # get start position of gene for hg19
 get_start_position <- function(n){
-  id <- getBM("start_position", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-  return(as.numeric(id))
+  id <- getBM("start_position", filters="hgnc_symbol", values=gene_file$name[n], mart=ensembl)
+  return(as.numeric(id[[1]]))
 }
-gene_file$start_position_on_the_genomic_accession <- sapply(1:dim(gene_file)[1], get_start_position)
+gene_file$start_position <- lapply(1:dim(gene_file)[1], get_start_position)
 # get end position of gene for hg19
 get_end_position <- function(n){
-  id <- getBM("end_position", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-  return(as.numeric(id))
+  id <- getBM("end_position", filters="hgnc_symbol", values=gene_file$name[n], mart=ensembl)
+  return(as.numeric(id[[1]]))
 }
-gene_file$end_position_on_the_genomic_accession <- sapply(1:dim(gene_file)[1], get_end_position)
+gene_file$end_position <- sapply(1:dim(gene_file)[1], get_end_position)
 # get percentage of GC content
 get_percentage_gc_content <- function(n){
-  id <- getBM("percentage_gene_gc_content", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-  return(id)
+  id <- getBM("percentage_gene_gc_content", filters="hgnc_symbol", values=gene_file$name[n], mart=ensembl)
+  return(id[[1]])
 }
 gene_file$percentage_gc_content <- sapply(1:dim(gene_file)[1], get_percentage_gc_content)
 
-# get phenotype description
-get_phenotype_description <- function(n){
-  id <- getBM("phenotype_description", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-  if(length(id) == 0){return("NA")}
-  return(id)
-}
-gene_file$phenotype_description <- sapply(1:dim(gene_file)[1], get_phenotype_description)
-gene_file$phenotype_description <- sapply(gene_file$phenotype_description, function(x) {paste(x, collapse = "; ")})
-
+gene_file <- gene_file[c("name", "description", "phenotype", "summary", "geneID", "EnsemblID", "mim",
+                 "map.location", "chromosome", "start_position", "end_position", "exon.count", "percentage_gc_content")]
 
 
 # UniProt/swissprot
-
-get_uniprotswissprot <- function(n){
-  id <- getBM("uniprotswissprot", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-  return(id)
-}
-gene_file$uniprotswissprot<- sapply(1:dim(gene_file)[1], get_uniprotswissprot)
-
-
-
-
-
-# # get go ID
-# get_go_id <- function(n){
-#   id <- getBM("go_id", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
+# get_uniprotswissprot <- function(n){
+#   id <- getBM("uniprotswissprot", filters="hgnc_symbol", values=gene_file$name[n], mart=ensembl)
 #   return(id)
 # }
-# gene_file$go_id <- sapply(1:dim(gene_file)[1], get_go_id)
-# # get go name
-# get_name_1006 <- function(n){
-#   id <- getBM("name_1006", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-#   return(id)
-# }
-# gene_file$name_1006 <- sapply(1:dim(gene_file)[1], get_name_1006)
-# # get go definition
-# get_definition_1006 <- function(n){
-#   id <- getBM("definition_1006", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-#   return(id)
-# }
-# gene_file$definition_1006 <- sapply(1:dim(gene_file)[1], get_definition_1006)
+# gene_file$uniprotswissprot<- sapply(1:dim(gene_file)[1], get_uniprotswissprot)
 
-# get GOSlim GOA Accession(s)
-# get_goslim_goa_accession <- function(n){
-#   id <- getBM("goslim_goa_accession", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-#   return(id)
-# }
-# gene_file$goslim_goa_accession <- sapply(1:dim(gene_file)[1], get_goslim_goa_accession)
-# # get GOSlim GOA Descriptions
-# get_goslim_goa_description <- function(n){
-#   id <- getBM("goslim_goa_description", filters="hgnc_symbol", values=gene_file$Symbol[n], mart=ensembl)
-#   return(id)
-# }
-# gene_file$goslim_goa_description <- sapply(1:dim(gene_file)[1], get_goslim_goa_description)
-
-gwasgene <- subsetByTraits(ebicat37, tr="Chronic kidney disease"  )
