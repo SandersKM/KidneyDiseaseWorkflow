b083a60c10e0408ba020d7c5a57f87dc3ce6837b
Kate Sanders <SandersKM@Hendrix.edu>
2018-07-24 13:06:33 -0400

    Runtime down to 11 sec!

--- GatherDiseases.R
+++ GatherDiseases.R
@@ -3,6 +3,7 @@ library(rvest)
 library(httr)
 library(rentrez)
 library(data.table)
+library(XML)
 
 ##########################################
 # Put Disease/Phenotype of interest below
@@ -20,7 +21,7 @@ disease.search.count <- entrez_search(db="medgen", term=disease.keyword, retmax=
 disease.search <- entrez_search(db="medgen", term=disease.keyword, retmax=disease.search.count)
 
 # makes a data frame for the disease results
-disease_file <- data.frame(id=disease.search$ids)
+disease_file <- data.frame(id=disease.search$ids, stringsAsFactors = FALSE)
 disease_file$summary <- entrez_summary(db="medgen", id =disease_file$id)
 disease_file$conceptid <- extract_from_esummary(disease_file$summary, "conceptid")
 disease_file$title <- extract_from_esummary(disease_file$summary, "title")
@@ -29,23 +30,18 @@ disease_file$definition <- unlist(lapply(disease_file$definition, toString))
 disease_file <- disease_file[ , !(names(disease_file) %in% c("summary"))]
 disease_genes <- entrez_link(dbfrom = "medgen", id = disease_file$id, db= 'gene')$links[1]$medgen_gene_diseases
 disease_genes_symbols <- extract_from_esummary(entrez_summary(db="gene", id = disease_genes), c("uid", "name"))
-demo_gene <- entrez_link(dbfrom = "medgen", id = disease_file$id, db= 'gene',by_id = TRUE)
-disease_file$demo_gene <- character(dim(disease_file)[1])
-get_genes <- function(n){
-  return(entrez_link(dbfrom="medgen", id = disease_file$id[n], db='gene')$links)
-}
-disease_file$genes <- sapply(1:dim(disease_file)[1], get_genes)
+disease_gene_by_id <- entrez_link(dbfrom = "medgen", id = disease_file$id, db= 'gene',by_id = TRUE)
 disease_file$genes_symbol <- character(dim(disease_file)[1])
-make_string_list <- function(n){
-  if(length(disease_file$genes[[n]]) == 0){
-    return("NULL")
-  }
-  disease_file$genes_symbol[n] <<- paste0(unlist(disease_genes_symbols["name",disease_file$genes[[n]]$medgen_gene_diseases]), collapse = "; ") 
-  return(paste(disease_file$genes[[n]]$medgen_gene_diseases, collapse="; "))
+disease_file$genes <- character(dim(disease_file)[1])
+for( i in 1:dim(disease_file)[1]){
+  rownum <- match(xmlValue(disease_gene_by_id[i][[1]]$file[2]$IdList[1]$Id[1]$text), disease_file$id)
+  disease_file$genes[rownum] <-
+    paste0(disease_gene_by_id[i][[1]]$links$medgen_gene_diseases, collapse = "; ")
+  disease_file$genes_symbol[rownum] <- paste0(disease_genes_symbols["name",disease_gene_by_id[i][[1]]$links$medgen_gene_diseases], collapse = "; ") 
 }
+
 # take out diseases without associated genes
-disease_file$genes <- sapply(1:dim(disease_file)[1], make_string_list)
-disease_file <- disease_file[!disease_file$genes == "NULL",]
+disease_file <- disease_file[!disease_file$genes == "",]
 disease_file <- disease_file[ , !(names(disease_file) %in% c("summary"))]
 
 get_inheritance <- function(n){
