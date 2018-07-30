73276155e736fec5a40d30eb37557befd7afc9a0
Kate Sanders <SandersKM@Hendrix.edu>
2018-07-30 13:44:44 -0400

    File Writing options

--- GatherDiseases.R
+++ GatherDiseases.R
@@ -4,17 +4,29 @@ library(httr)
 library(rentrez)
 library(data.table)
 library(XML)
+library(xlsx)
 
-##########################################
+##############
+# User Input
+##############
+
 # Put Disease/Phenotype of interest below
-##########################################
-
 disease.keyword = "Kidney Disease"
 
 # Enter the path you would like the final CSV to be in:
 disease_file_path = "/Users/ksanders/Documents/"
-disease_file_name = paste(sub(" ", "_",disease.keyword),  ".csv", sep = "")
 
+
+# Set to true what file formats you want:
+write_to_excel <- TRUE
+write_to_csv <- TRUE
+write_to_tsv <- TRUE
+
+disease_file_name = paste("Mendelian_Pipeline_",sub(" ", "_", disease.keyword), sep = "")
+
+################
+# MedGen Search
+################
 
 # API lookup
 disease.search.count <- entrez_search(db="medgen", term=disease.keyword, retmax=0)$count
@@ -33,6 +45,7 @@ disease_genes_symbols <- extract_from_esummary(entrez_summary(db="gene", id = di
 disease_gene_by_id <- entrez_link(dbfrom = "medgen", id = disease_file$id, db= 'gene',by_id = TRUE)
 disease_file$genes_symbol <- character(dim(disease_file)[1])
 disease_file$genes <- character(dim(disease_file)[1])
+# put together genes and gene symbol information for each disease 
 for( i in 1:dim(disease_file)[1]){
   rownum <- match(xmlValue(disease_gene_by_id[i][[1]]$file[2]$IdList[1]$Id[1]$text), disease_file$id)
   disease_file$genes[rownum] <-
@@ -41,9 +54,11 @@ for( i in 1:dim(disease_file)[1]){
 }
 
 # take out diseases without associated genes
-disease_file <- disease_file[!disease_file$genes == "",]
-disease_file <- disease_file[ , !(names(disease_file) %in% c("summary"))]
+disease_file <- disease_file[!disease_file$genes == "",] 
+# Get rid of Summary Column
+disease_file <- disease_file[ , !(names(disease_file) %in% c("summary"))] 
 
+# Get inheritance type from disease summary
 get_inheritance <- function(n){
   def <- strsplit(tolower(n), split = " ")
   if(length(def) > 0){
@@ -65,8 +80,20 @@ get_inheritance <- function(n){
 disease_file$inheritance <- sapply(disease_file$definition, get_inheritance)
 disease_file$inheritance <- unlist(disease_file$inheritance)
 
-write.csv(disease_file, file=paste(disease_file_path, disease_file_name, sep=""), row.names = FALSE)
+#################
+# Write to file
+#################
 
-# you should go back and add in the names of the related genes. 
-# Also make one excel file with multiple sheets
+# Write results to csv
+if(write_to_csv){
+  write.csv(disease_file, file=paste(disease_file_path, disease_file_name, ".csv", sep=""), row.names = FALSE)
+}
+if(write_to_tsv){
+  write.table(disease_file, file = paste(disease_file_path, disease_file_name, ".txt", sep=""), sep = "\t", row.names = FALSE)
+}
+if(write_to_excel){
+  write.xlsx2(disease_file, file=paste(disease_file_path, disease_file_name, ".xlsx", sep=""), sheetName = "Disease", row.names = FALSE)
+}
+
+
 end_time <- Sys.time()
